260c0eabeba8fe0fbd37a211f10dfee474dc18b0
Sergey <vs-maykop@mail.ru>
2024-12-22 17:22:38 +0300

    KS-1-3 lesson one, task three

---
+++ src/main/kotlin/lesson_1/task_3.kt
@@ -0,0 +1,17 @@
+package org.example.lesson_1
+
+fun main() {
+    val yearGagarinFlight = 1961
+    var hour = "09"
+    var minute = "07"
+
+    println(yearGagarinFlight)
+    //println(hour)
+    //println(minute)
+
+    hour = "10"
+    minute = "55"
+    print(hour)
+    print(':')
+    print(minute)
+}
