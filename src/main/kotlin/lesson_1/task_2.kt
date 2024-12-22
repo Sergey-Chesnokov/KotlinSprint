94064e4fb516566f05207af70238378962dd63d7
Sergey <vs-maykop@mail.ru>
2024-12-22 17:11:01 +0300

    KS-1-2 lesson one, task two

---
+++ src/main/kotlin/lesson_1/task_2.kt
@@ -0,0 +1,15 @@
+package org.example.lesson_1
+
+fun main() {
+    val numberOfOrders: Int = 75
+    val thanksForOrder: String = "Спасибо за покупку!"
+
+    println(numberOfOrders)
+    println(thanksForOrder)
+    println()
+
+    var numbersOfEmployeesInTheStore = 2000
+    //println(numbersOfEmployeesInTheStore)
+    numbersOfEmployeesInTheStore = 1999
+    println(numbersOfEmployeesInTheStore)
+}
