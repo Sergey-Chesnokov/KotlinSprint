200d7181b3b79b9674fb1d24aa03b6a77d7a83cb
Sergey <vs-maykop@mail.ru>
2024-12-23 21:40:57 +0300

    KS-1-5 lesson one, task five

--- src/main/kotlin/lesson_1/task_5.kt
+++ src/main/kotlin/lesson_1/task_5.kt
@@ -1,17 +1,15 @@
 package org.example.lesson_1
 
 fun main() {
-    var seconds: Short = 6480
-    var HOURS = 3600 // 3600 секунд в часе
-    var flightTimeOfTheHours = seconds / HOURS
-    var MINUTE = 60 // секунд в минуте
-    var flightTimeOfTheMinutes = (seconds % HOURS) / MINUTE // (остаток от деления общего количсетва секунд на количество секунд в часе) разделить на количество сикунд в минту
-    var flightTimeOfTheSeconds = seconds % 60 // остаток в секундах который не поместился в минутах
+    val seconds: Short = 6480
+    val flightTimeOfTheHours = seconds / HOURS
+    val flightTimeOfTheMinutes = (seconds % HOURS) / MINUTE // (остаток от деления общего количсетва секунд на количество секунд в часе) разделить на количество сикунд в минту
+    val flightTimeOfTheSeconds = seconds % MINUTE // остаток в секундах который не поместился в минутах
 
-    var flightTime = String.format("%02d:%02d:%02d", flightTimeOfTheHours, flightTimeOfTheMinutes, flightTimeOfTheSeconds)
+    val flightTime = String.format("%02d:%02d:%02d", flightTimeOfTheHours, flightTimeOfTheMinutes, flightTimeOfTheSeconds)
 
     println(flightTime)
-
-
-
 }
+
+const val  HOURS = 3600 // 3600 секунд в часе
+const val MINUTE = 60 // секунд в минуте
